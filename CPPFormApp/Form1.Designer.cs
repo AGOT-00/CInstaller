885c5ce4ac7916efcd6d68d156efa98f4aa3624e
Muhammad Talha Zulfiqar <talhazulf4163@gmail.com>
2023-02-09 01:05:27 +0500

    Add project files.

---
+++ CPPFormApp/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CPPFormApp
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DownloadBar = new System.Windows.Forms.ProgressBar();
+            this.Status = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Down = new System.Windows.Forms.Label();
+            this.DSpeed = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DownloadBar
+            // 
+            this.DownloadBar.Location = new System.Drawing.Point(41, 12);
+            this.DownloadBar.Name = "DownloadBar";
+            this.DownloadBar.Size = new System.Drawing.Size(537, 23);
+            this.DownloadBar.TabIndex = 2;
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(39, 63);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(46, 13);
+            this.Status.TabIndex = 3;
+            this.Status.Text = "Status : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Speed :";
+            // 
+            // Down
+            // 
+            this.Down.AutoSize = true;
+            this.Down.Location = new System.Drawing.Point(91, 63);
+            this.Down.Name = "Down";
+            this.Down.Size = new System.Drawing.Size(73, 13);
+            this.Down.TabIndex = 5;
+            this.Down.Text = "Downloaded :";
+            // 
+            // DSpeed
+            // 
+            this.DSpeed.AutoSize = true;
+            this.DSpeed.Location = new System.Drawing.Point(91, 38);
+            this.DSpeed.Name = "DSpeed";
+            this.DSpeed.Size = new System.Drawing.Size(45, 13);
+            this.DSpeed.TabIndex = 6;
+            this.DSpeed.Text = "MB/sec";
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(503, 41);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 7;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 100);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.DSpeed);
+            this.Controls.Add(this.Down);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.DownloadBar);
+            this.Name = "App";
+            this.Text = "C++ Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar DownloadBar;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Down;
+        private System.Windows.Forms.Label DSpeed;
+        private System.Windows.Forms.Button Start;
+    }
+}
+
---
+++ CPPFormApp/Form1.cs
@@ -0,0 +1,148 @@
+﻿using AltoHttp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CPPFormApp
+{
+
+    public partial class App : Form
+    {
+
+        HttpDownloader Downloader;
+        public String Url = "https://github.com/AGOT-00/others/raw/master/Mingw.zip";
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Downloader == null)
+                {
+                    Downloader = new HttpDownloader(Url, "./Mingw.zip");
+
+                    Downloader.DownloadCompleted += Downloader_DownloadCompleted;
+                    Downloader.ProgressChanged += Downloader_DownloadProgressChanged;
+
+                    Downloader.Start();
+
+                    Down.Text = "Downloading the required files.";
+                }
+                else
+                {
+                    Down.Text = "Already Downloading Kindly Wait.";
+                }
+            }
+            catch(Exception en)
+            {
+                Down.Text = en.Message;
+            }
+            
+        }
+
+        private void Downloader_DownloadCompleted(object sender, EventArgs e)
+        {
+            if (Downloader != null)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Down.Text = "Downloading Completed.";
+                    DownloadBar.Value = 100;
+                }
+           );
+                this.extractzipfile();
+            }
+        }
+        private void Downloader_DownloadProgressChanged(object sender, AltoHttp.ProgressChangedEventArgs e)
+        {
+            DownloadBar.Value = (int)e.Progress;
+            DSpeed.Text = String.Format("{0} MB/sec", (e.SpeedInBytes / 1024d / 1024d).ToString("0.00"));
+        }
+        
+        public void extractzipfile()
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                Down.Text = "Downloading Completed.Unzipping the Zip Files.";
+            }
+            );
+            try
+            {
+                ZipFile.ExtractToDirectory("./Mingw.zip", "./");
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Down.Text = "Unzipping Completed.";
+                }
+                );
+            }
+            catch (Exception e)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Down.Text = "Unzipping Error. MayBe Folder Already exists.";
+                }
+                );
+            }
+
+            string sourceDirName = @"./Mingw";
+            string destDirName = @"C:\Mingw";
+            try
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Down.Text = "Moving Files to Required Directory.";
+                }
+                );
+
+                Directory.Move(sourceDirName, destDirName);
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Down.Text = "Moving Files Completed.";
+                }
+                );
+            }
+            catch (IOException exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+
+            try
+            {
+                var name = "PATH";
+                var scope = EnvironmentVariableTarget.Machine;
+                var oldValue = Environment.GetEnvironmentVariable(name, scope);
+
+                var newValue = oldValue + ";" + destDirName + @"\bin\";
+                Environment.SetEnvironmentVariable(name, newValue, scope);
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Down.Text = "Installation Completed.";
+                }
+               );
+            }
+            catch(Exception e)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Down.Text = "Error Occur While Setting Environment.";
+                }
+               );
+            }
+        }
+    }
+}
